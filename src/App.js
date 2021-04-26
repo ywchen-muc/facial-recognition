c5f1e8f14eff511e941d0d232c5ae43280c0f8b4
Vivienne Chen <viviennechen@Viviennede-MBP.fritz.box>
2021-04-26 16:07:40 +0200

    adjust to detect multiple faces

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
       }
       return boxes;
     // const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    // const image = document.getElementById('inputimage');
+    // const image = document.getElementById('inputImage');
     // const width = Number(image.width);
     // const height = Number(image.height);
     // return {
